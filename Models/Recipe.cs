424ac8a36fff5c0b87444fc7e627bc1537bf6c0f
Custodia08 <167979099+Custodia08@users.noreply.github.com>
2025-05-20 22:49:21 +0300

    Model değişikliği yapıldı. Migration düzenlendi.

--- Models/Recipe.cs
+++ Models/Recipe.cs
@@ -12,5 +12,9 @@ namespace Yemek_Tarifi_Site.Models
         public string? RecipeExp { get; set; }
         [Required(ErrorMessage = "Recipe has to have materials!")]
         public List<string>? RecipeMaterial { get; set; }
+        [Required(ErrorMessage = "It should be known how many people the recipe is for!")]
+        public string? RecipeHowPerson { get; set; }
+        [Required(ErrorMessage = "It should be known how much time it takes the recipe is for!")]
+        public string? RecipePrepTime { get; set; }
     }
 }
--- obj/Debug/net8.0/Yemek-Tarifi-Site.AssemblyInfo.cs
+++ obj/Debug/net8.0/Yemek-Tarifi-Site.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Yemek-Tarifi-Site")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9450e48fd36c84ca65316b2370d3fba7cf1fe890")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55b7e30133129d0378d427718e9b8d9306edf000")]
 [assembly: System.Reflection.AssemblyProductAttribute("Yemek-Tarifi-Site")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Yemek-Tarifi-Site")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
